11fc5d8f86fa8f57723ecb646556d06f416f866d
Juan David Perez <juandavidperezvergara@gmail.com>
2023-08-16 19:52:31 -0500

    Ignorando archivos

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ import "firebase/database";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+//
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
