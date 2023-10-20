d84d4e26227fda34a7a514a676e8c84ccb54b088
Juan David Perez <juandavidperezvergara@gmail.com>
2023-10-19 20:56:32 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDMkHmAijQ6yBmvSgrjmrkUFM_GQr6CJO4",
+  apiKey: "ApiKey",
   authDomain: "fitplan-503ef.firebaseapp.com",
   databaseURL: "https://fitplan-503ef-default-rtdb.firebaseio.com",
   projectId: "fitplan-503ef",
   storageBucket: "fitplan-503ef.appspot.com",
   messagingSenderId: "407184834576",
-  appId: "1:407184834576:web:55e389c205ee0e24d44a95",
+  appId: "appId",
   measurementId: "G-8GWQVMGWQ4",
 };
 
