d18b763c1c012ad05d4b90bbc0323ab8e84f7812
Juan David Perez <juandavidperezvergara@gmail.com>
2023-10-23 20:48:49 -0500

    removing secrets

--- firebase.js
+++ firebase.js
@@ -9,7 +9,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //no
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
